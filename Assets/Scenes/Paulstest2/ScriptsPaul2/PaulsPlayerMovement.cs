4bc4136ae2faf7c6b735ac919b3e51b3ce17c418
IBinooI <paulr70@hotmail.com>
2022-11-16 21:35:24 -0500

    Added in first person character camera and mouse

---
+++ Assets/Scenes/Paulstest2/ScriptsPaul2/PaulsPlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaulsPlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+
+}
