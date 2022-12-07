559165a8dc5d35fafe235486114bf50b1f2f3008
Cole <coleportolesi@gmail.com>
2022-12-06 21:55:03 -0500

    NOT FINISHED, Roomlist updates

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
+
+/// COMMENT THE CODE
+/// COMMENT THE CODE  
+/// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE /// COMMENT THE CODE
+/// COMMENT THE CODE 
+
 
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
 
+    void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -60,9 +83,26 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
     }
 
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
 }
